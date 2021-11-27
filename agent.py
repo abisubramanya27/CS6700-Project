f34757fdeb5fd955cb8e8f3b7a69a91d8e4b9713
Anish <anish@pop-os.localdomain>
2021-11-27 18:28:07 +0530

    np to list+ some changes to init

--- agent.py
+++ agent.py
@@ -24,6 +24,10 @@ Use the state saved in train phase here.
 
 
 class Agent:
+    def argmax(self, lst):
+        #return np.argmax(np.array(lst))
+        return lst.index(max(lst))
+
     def convert_a(self, state):
         a1 = (state[0]+1)*self.nbins//2
         if a1 == self.nbins:
@@ -38,11 +42,21 @@ class Agent:
         if a4 == self.nbins:
             a4 -= 1
         return int(a1*(self.nbins ** 3) + a2*(self.nbins ** 2) + a3 *(self.nbins) + a4) 
+    
     def convert_kbc(self, state):
         if "" in state: 
             return state.index("")
         else:
             return len(state)
+    
+    def convert(self, state):
+        #print(state)
+        if self.config[0] == 0:
+            return self.convert_a(state)
+        elif self.config[0] == 1:
+            return state
+        else:
+            return self.convert_kbc(state)
 
     def __init__(self, env):
         self.env_name = env
@@ -50,16 +64,39 @@ class Agent:
         self.n_episodes = 0
         self.prev_action = None
         self.prev_obs = None
-        self.epsilon = 0.3
-        self.gma = 0.9   # earlier 0.6
-        self.eta = 0.2  # earlier 0.1
+        epsilon_dict = {
+                0: 0.6,
+                1: 0.3,
+                2: 0.3,
+                3: 0.3,
+                4: 0.3
+                }
+        self.epsilon = epsilon_dict[self.config[0]]
+        gamma_dict = {
+                0: 0.9,
+                1: 0.9,
+                2: 0.9,
+                3: 0.9,
+                4: 0.9
+                }
+        self.gma = gamma_dict[self.config[0]]   
+        eta_dict = {
+                0: 0.2,
+                1: 0.3,
+                2: 0.2,
+                3: 0.2,
+                4: 0.2
+                }
+        self.eta = eta_dict[self.config[0]]   
         if self.config[0]:
             self.Q = np.zeros((self.config[1], self.config[2]))
+            #self.Q = [[0]*self.config[2]]*self.config[1]
             self.n_obs_space = self.config[1]
             self.n_action_space = self.config[2]
         else:
-            self.nbins = 5
-            self.Q = np.zeros((self.nbins ** 4+1, 3))
+            self.nbins = 4
+            #self.Q = np.zeros((self.nbins ** 4+1, 3))
+            self.Q = [[0]*3]*(self.nbins ** 4+1)
             self.n_action_space = self.config[2]
 
     def register_reset_train(self, obs):
@@ -72,15 +109,13 @@ class Agent:
             - action - discretized 'action' from raw 'observation'
         """
 
-        if not self.config[0]:
-            obs = self.convert_a(obs)
-        if self.config[0] >= 2:
-            obs = self.convert_kbc(obs)
+        obs = self.convert(obs)
         #print(obs)
         if random.uniform(0, 1) < self.epsilon:
             self.prev_action = random.randint(0, self.n_action_space-1)
         else:
             self.prev_action = np.argmax(self.Q[obs,:])
+            #self.prev_action = self.argmax(self.Q[obs])
 
         self.prev_obs = obs
         self.n_episodes += 1
@@ -89,7 +124,7 @@ class Agent:
     def compute_action_train(self, obs, reward, done, info):
         """
         Use this function in the train phase
-        This function is called at all subsequent steps of an episode until done=True
+        This function is called at all subsequent st]ps of an episode until done=True
         PARAMETERS  : 
             - observation - raw 'observation' from environment
             - reward - 'reward' obtained from previous action
@@ -99,19 +134,17 @@ class Agent:
         RETURNS     : 
             - action - discretized 'action' from raw 'observation'
         """
-        if not self.config[0]:
-            obs = self.convert_a(obs)
-        if self.config[0] >= 2:
-            obs = self.convert_kbc(obs)
+        obs = self.convert(obs)
         #print(obs, self.prev_action)
-        self.Q[self.prev_obs,self.prev_action] = self.Q[self.prev_obs,self.prev_action] + \
-            self.eta*(reward + self.gma*np.max(self.Q[obs,:]) - \
-                self.Q[self.prev_obs,self.prev_action])
+        self.Q[self.prev_obs,self.prev_action] = self.Q[self.prev_obs,self.prev_action] + self.eta*(reward + self.gma*np.max(self.Q[obs,:]) - self.Q[self.prev_obs,self.prev_action])
+
+        #self.Q[self.prev_obs][self.prev_action] = self.Q[self.prev_obs][self.prev_action] + self.eta*(reward + self.gma*max(self.Q[obs]) - self.Q[self.prev_obs][self.prev_action])
 
         if random.uniform(0, 1) < self.epsilon:
             self.prev_action = random.randint(0, self.n_action_space-1)
         else:
             self.prev_action = np.argmax(self.Q[obs,:])
+            #self.prev_action = self.argmax(self.Q[obs])
         
         self.prev_obs = obs
         return self.prev_action
@@ -126,11 +159,10 @@ class Agent:
             - action - discretized 'action' from raw 'observation'
         """
 
-        if not self.config[0]:
-            obs = self.convert_a(obs)
-        if self.config[0] >= 2:
-            obs = self.convert_kbc(obs)
+        obs = self.convert(obs)
+        #print(self.Q)
         action = np.argmax(self.Q[obs,:])
+        #action = self.argmax(self.Q[obs])
         return action
 
     def compute_action_test(self, obs, reward, done, info):
@@ -147,9 +179,8 @@ class Agent:
             - action - discretized 'action' from raw 'observation'
         """
 
-        if not self.config[0]:
-            obs = self.convert_a(obs)
-        if self.config[0] >= 2:
-            obs = self.convert_kbc(obs)
+        obs = self.convert(obs)
         action = np.argmax(self.Q[obs,:])
+        #action = self.argmax(self.Q[obs])
+
         return action
