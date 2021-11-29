abf570bbc64496aa3bd065e820ff9b87a5eaa13a
Abishek S <abisubramanya27@gmail.com>
2021-11-29 13:21:03 +0000

    new hparam for kbcb, kbca

--- agent.py
+++ agent.py
@@ -203,19 +203,25 @@ class Agent:
             # self.eta = min(5e-6 * 2 ** self.n_step, 0.5)
             self.eps = max(3 / (3 + self.n_step), 0.1)
             if self.actions[-1] == 1:
-                reward += (0.5 ** (self.states[-2][0]) - self.gma * 0.5 ** (state[0])) * 1e10
+                reward += (0.5 ** (self.states[-2][0])) * 1e9
+            else:
+                reward += 1e5
         
         elif self.env_name == 'kbcb':
             # self.eta = min(5e-6 * 2 ** self.n_step, 0.5)
             self.eps = max(3 / (3 + self.n_step), 0.1)
             if self.actions[-1] == 1:
-                reward += (0.5 ** (self.states[-2][0])) * 1e7
+                reward += (0.5 ** (self.states[-2][0])) * 1e9
+            else:
+                reward += 1e2
         
         elif self.env_name == 'kbcc':
             # self.eta = min(5e-6 * 2 ** self.n_step, 0.5)
             self.eps = max(3 / (3 + self.n_step), 0.1)
             if self.actions[-1] == 1:
                 reward += (0.5 ** (self.states[-2][0]) - self.gma * 0.5 ** (state[0])) * 1e8
+            else:
+                reward += 1e5
         
         elif self.env_name == 'taxi':
             self.eta = max(80 / (80 + self.n_step), 0.5)
@@ -255,9 +261,9 @@ class Agent:
 
             for i in range(len(self.rewards)):
                 if self.env_name == 'kbca':
-                    self.alpha = min(1e-9 * 2 ** (i), 1e-7)
+                    self.alpha = min(5e-9 * 2 ** (i), 1e-6)
                 elif self.env_name == 'kbcb':
-                    self.alpha = min(5e-8 * 2 ** (i), 1e-6)
+                    self.alpha = min(5e-9 * 2 ** (i), 1e-6)
                 elif self.env_name == 'kbcc':
                     self.alpha = min(1e-8 * 2 ** (i), 1e-6)
                 elif self.env_name == 'acrobot':
@@ -282,9 +288,6 @@ class Agent:
             action, _ = self.policy.act(state)
         else:
             action = np.argmax(self.Q[state])
-
-        if 'kbc' in self.env_name:
-            action = 1
 
         return action
 
